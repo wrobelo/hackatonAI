f881aeee30a76aaafb560da8a8ed22b08ddb2e87
adabrowski1 <adam.dabrowski@external.t-mobile.pl>
2025-04-25 20:22:26 +0200

    fix for api

--- agents-handler/main.py
+++ agents-handler/main.py
@@ -7,4 +7,4 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 if __name__ == '__main__':
-    uvicorn.run('app.api.routes:app', host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run('app.api:app', host='0.0.0.0', port=8070, reload=True)
