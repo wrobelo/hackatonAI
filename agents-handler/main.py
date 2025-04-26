3d41dc10edc78bcb429cf17ae30236d7e32d1bf6
adabrowski1 <adam.dabrowski@external.t-mobile.pl>
2025-04-26 03:42:26 +0200

    docker env

--- agents-handler/main.py
+++ agents-handler/main.py
@@ -6,5 +6,6 @@ import uvicorn
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
+
 if __name__ == '__main__':
     uvicorn.run('app.api:app', host='0.0.0.0', port=8070, reload=True)
