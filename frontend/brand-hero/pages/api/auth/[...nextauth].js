4eabac84401fb1a0c4ca146a22e519acc91c3cf1
Oskar Wrobel <oskar.wrobel@sciamus.com>
2025-04-26 15:09:17 +0200

    frontend - logowanie do fb

--- frontend/brand-hero/pages/api/auth/[...nextauth].js
+++ frontend/brand-hero/pages/api/auth/[...nextauth].js
@@ -5,7 +5,13 @@ export const authOptions = {
     providers: [
         FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+            authorization: {
+                url: "https://www.facebook.com/v17.0/dialog/oauth",
+                params: {
+                    scope: "email public_profile pages_show_list pages_read_engagement pages_manage_posts"
+                }
+            }
         })
     ],
 }
